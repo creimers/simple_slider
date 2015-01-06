c1547f74b24fcb6b180c5f0e73fe27c0c886699f
Christoph Reimers <christoph@superservice-international.com>
2015-01-06 10:15:12 +0100

    refined cms_plugins.py

--- cms_plugins.py
+++ cms_plugins.py
@@ -2,17 +2,27 @@
 
 from cms.plugin_pool import plugin_pool
 from cms.plugin_base import CMSPluginBase
+from adminsortable.admin import SortableStackedInline
 
 from django.utils.translation import ugettext as _
 
-from .models import SliderPluginModel
+from .models import (
+    SliderPluginModel,
+    Image
+)
+
+
+class ImageInline(SortableStackedInline):
+    model = Image
+    extra = 1
 
 
 class Slider(CMSPluginBase):
 
     model = SliderPluginModel
     name = _("Gallerie")
-    render_template = "djangocms_gallerij/_gallery.html"
+    render_template = "djangocms_simple_slider/_slider.html"
+    inlines = [ImageInline, ]
 
     def render(self, context, instance, placeholder):
         images = instance.slider.images.all()
